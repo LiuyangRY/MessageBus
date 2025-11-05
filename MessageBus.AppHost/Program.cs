8cc228c0a124047a0fdb7b605ebbc2831ebf5e25
Liuyang <1044457987@qq.com>
2025-11-05 16:04:29 +0800

    添加rabbitmq容器

--- MessageBus.AppHost/Program.cs
+++ MessageBus.AppHost/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.MessageBus_ApiService>("apiservice");
@@ -6,4 +8,49 @@ builder.AddProject<Projects.MessageBus_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(apiService);
 
+
+var rabbitmqUserName = builder.AddParameter("rabbitmq-username", "admin");
+var rabbitmqPassword = builder.AddParameter("rabbitmq-password", "admin123");
+var upstreamRabbitmq = builder.AddRabbitMQ("upstream-rabbitmq", rabbitmqUserName, rabbitmqPassword)
+    .WithImage("rabbitmq:management")
+    .WithContainerName("upstreamRabbitmq")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("TZ", "Asia/Shanghai")
+    .WithEnvironment("RABBITMQ_DEFAULT_USER", rabbitmqUserName)
+    .WithEnvironment("RABBITMQ_DEFAULT_PASS", rabbitmqPassword)
+    .WithEnvironment("RABBITMQ_LOOPBACK_USERS", "[]")
+    .WithEndpoint("rabbit-management", endpoint =>
+    {
+        endpoint.Port = 24000;
+        endpoint.TargetPort = 15672;
+    });
+var messageBusRabbitmq = builder.AddRabbitMQ("messagebus-rabbitmq", rabbitmqUserName, rabbitmqPassword)
+    .WithImage("rabbitmq:management")
+    .WithContainerName("messageBusRabbitmq")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("TZ", "Asia/Shanghai")
+    .WithEnvironment("RABBITMQ_DEFAULT_USER", rabbitmqUserName)
+    .WithEnvironment("RABBITMQ_DEFAULT_PASS", rabbitmqPassword)
+    .WithEnvironment("RABBITMQ_LOOPBACK_USERS", "[]")
+    .WithEndpoint("rabbit-management", endpoint =>
+    {
+        endpoint.Port = 25000;
+        endpoint.TargetPort = 15672;
+    });
+var downstreamRabbitmq = builder.AddRabbitMQ("downstream-rabbitmq", rabbitmqUserName, rabbitmqPassword)
+    .WithImage("rabbitmq:management")
+    .WithContainerName("downstreamRabbitmq")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithExternalHttpEndpoints()
+    .WithEnvironment("TZ", "Asia/Shanghai")
+    .WithEnvironment("RABBITMQ_DEFAULT_USER", rabbitmqUserName)
+    .WithEnvironment("RABBITMQ_DEFAULT_PASS", rabbitmqPassword)
+    .WithEnvironment("RABBITMQ_LOOPBACK_USERS", "[]")
+    .WithEndpoint("rabbit-management", endpoint =>
+    {
+        endpoint.Port = 26000;
+        endpoint.TargetPort = 15672;
+    });
 builder.Build().Run();
